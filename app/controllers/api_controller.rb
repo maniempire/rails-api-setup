5cd1e0aa0ffc2dcc62a9f5df74790c4692433594
Manimaran Malaichamy <mani@maniempire.com>
2016-10-04 18:10:56 +0530

    add token to authenticate user

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,24 @@
+class ApiController < ApplicationController 
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user
+    
+  private
+  def authenticate_user
+    user_token = request.headers['X-USER-TOKEN']
+    if user_token
+      @user = User.find_by_token(user_token)
+      #Unauthorize if a user object is not returned
+      if @user.nil?
+        return unauthorize
+      end
+    else
+      return unauthorize
+    end
+  end
+
+  def unauthorize
+    head status: :unauthorized
+    return false
+  end
+        
+end
